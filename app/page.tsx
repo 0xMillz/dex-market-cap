ea559fb0264d958e4dbd5374fa7b6c0711e20588
Mills McIlroy <37815163+millsmcilroy@users.noreply.github.com>
2023-06-13 22:00:48 -0600

    update homepage text

--- app/page.tsx
+++ app/page.tsx
@@ -12,16 +12,14 @@ export const dynamic = 'force-dynamic'
 export default function Home() {
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center">
-      <Link
-        href="https://vercel.com/templates/next.js/postgres-starter"
-        className="group mt-20 sm:mt-0 rounded-full flex space-x-1 bg-white/30 shadow-sm ring-1 ring-gray-900/5 text-gray-600 text-sm font-medium px-10 py-2 hover:shadow-lg active:shadow-sm transition-all"
-      >
-        <p>Deploy your own to Vercel</p>
-        <ExpandingArrow />
-      </Link>
       <h1 className="pt-4 pb-8 bg-gradient-to-br from-black via-[#171717] to-[#575757] bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl">
-        Postgres on Vercel
+        DEX Market Cap
       </h1>
+      <p className="max-w-lg text-center mt-6">
+       Real-time, on-chain data or it didn't happen!
+      <br/>
+        Centralized exchanges need not apply.
+      </p>
       <Suspense fallback={<TablePlaceholder />}>
         {/* @ts-expect-error Async Server Component */}
         <Table />
